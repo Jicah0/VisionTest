12bbc9de29824e20e07031e0cf88571f7edb98ad
Jicah0 <109925131+Jicah0@users.noreply.github.com>
2025-01-10 18:31:45 -0800

    Update Vision.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Vision.java
@@ -23,15 +23,38 @@ import org.opencv.core.Scalar;
 import java.util.List;
 
 public class Vision extends SubsystemBase {
+
+    public enum Alliance {
+        RED,
+        BLUE
+    }
+
+    public static Alliance alliance = Alliance.BLUE;
+
     Telemetry telemetry;
 
-    ColorRange blue = new ColorRange(
-            ColorSpace.HSV,
-            new Scalar( 100, 40, 40),
-            new Scalar(140, 255, 255)
-    );
-    ColorBlobLocatorProcessor locatorProcess = new ColorBlobLocatorProcessor.Builder()
-            .setTargetColorRange(blue)
+//    ColorRange blue = new ColorRange(
+//            ColorSpace.HSV,
+//            new Scalar( 100, 80, 80),
+//            new Scalar(140, 255, 255)
+//    );
+
+    ColorRange blue = ColorRange.BLUE;
+
+    ColorRange red = ColorRange.RED;
+
+    ColorRange yellow = ColorRange.YELLOW;
+
+
+    ColorBlobLocatorProcessor.Builder allianceLocatorProcessBuilder = new ColorBlobLocatorProcessor.Builder()
+            .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+            .setRoi(ImageRegion.entireFrame())
+            .setBlurSize(5)
+            .setDrawContours(true);
+
+    ColorBlobLocatorProcessor allianceLocatorProcess;
+    ColorBlobLocatorProcessor yellowLocatorProcess = new ColorBlobLocatorProcessor.Builder()
+            .setTargetColorRange(yellow)
             .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
             .setRoi(ImageRegion.entireFrame())
             .setBlurSize(5)
@@ -40,22 +63,37 @@ public class Vision extends SubsystemBase {
 
     VisionPortal visionPortal;
 
+
     public Vision(CameraName camera, Telemetry telemetry){
         this.telemetry = telemetry;
 
+        if (alliance == Alliance.BLUE){
+            allianceLocatorProcessBuilder.setTargetColorRange(red);
+        }
+        else{
+            allianceLocatorProcessBuilder.setTargetColorRange(red);
+        }
+
+        allianceLocatorProcess = allianceLocatorProcessBuilder.build();
+
         ColorBlobLocatorProcessor.BlobFilter areaFilter =
-                new ColorBlobLocatorProcessor.BlobFilter(ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA, 30000, 150000);
-        locatorProcess.addFilter(areaFilter);
+                new ColorBlobLocatorProcessor.BlobFilter(ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA, 30000, 100000);
         ColorBlobLocatorProcessor.BlobFilter ratioFilter =
                 new ColorBlobLocatorProcessor.BlobFilter(ColorBlobLocatorProcessor.BlobCriteria.BY_ASPECT_RATIO, 1.35, 3);
-        locatorProcess.addFilter(ratioFilter);
         ColorBlobLocatorProcessor.BlobSort largestSort =
                 new ColorBlobLocatorProcessor.BlobSort(ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA, SortOrder.DESCENDING);
-        locatorProcess.setSort(largestSort);
+
+        for(ColorBlobLocatorProcessor process : new ColorBlobLocatorProcessor[]{allianceLocatorProcess, yellowLocatorProcess}){
+//            process.addFilter(areaFilter);
+//            process.addFilter(ratioFilter);
+            process.setSort(largestSort);
+        }
+
         visionPortal = new VisionPortal.Builder()
                 .setCamera(camera)
                 .setCameraResolution(new Size(640, 480))
-                .addProcessor(locatorProcess)
+                .addProcessor(yellowLocatorProcess)
+                .addProcessor(allianceLocatorProcess)
                 .build();
     }
 
@@ -64,12 +102,57 @@ public class Vision extends SubsystemBase {
         telemetry.addData("Sample Skew", getSampleSkew().orElse(-99999.0));
     }
 
+    /**
+     * Gets Sample Skew in Degrees
+     * @return Optional Double, can return Optional.empty to account for when vision doesn't see anything
+     */
     public Optional<Double> getSampleSkew(){
-        List<ColorBlobLocatorProcessor.Blob> blobs = locatorProcess.getBlobs();
+        List<ColorBlobLocatorProcessor.Blob> blobs = yellowLocatorProcess.getBlobs();
+        List<ColorBlobLocatorProcessor.Blob> allianceBlobs = allianceLocatorProcess.getBlobs();
 
-        if(blobs.size() == 0){return Optional.empty();}
+        if(blobs.isEmpty() && allianceBlobs.isEmpty()){return Optional.empty();}
 
-        RotatedRect boxFitBlob = blobs.get(0).getBoxFit();
+        RotatedRect boxFitBlob;
+        //I know all of this is dookie but this is the fastest most reliable way to get results as of now with my current knowledge
+        if (blobs.isEmpty()){
+            boxFitBlob = allianceBlobs.get(0).getBoxFit();
+        }
+        else if (allianceBlobs.isEmpty()){
+            boxFitBlob = blobs.get(0).getBoxFit();
+        }
+        else{
+            RotatedRect boxFitYellow = blobs.get(0).getBoxFit();
+            RotatedRect boxFitAlliance = allianceBlobs.get(0).getBoxFit();
+            double yellowArea = boxFitYellow.size.height * boxFitYellow.size.width;
+            double allianceArea = boxFitAlliance.size.height * boxFitAlliance.size.width;
+            if (yellowArea > allianceArea){
+                boxFitBlob = boxFitYellow;
+            }
+            else {
+                boxFitBlob = boxFitAlliance;
+            }
+        }
+
+
+//        boxFitBlob = blobs.get(0).getBoxFit();
+        return Optional.of(getAngleFromRotatedRect(boxFitBlob));
+    }
+
+    public Optional<Double> getYellowSkew(){
+        List<ColorBlobLocatorProcessor.Blob> blobs = yellowLocatorProcess.getBlobs();
+        if(blobs.isEmpty()){return Optional.empty();}
+
+        return Optional.of(getAngleFromRotatedRect(blobs.get(0).getBoxFit()));
+    }
+
+    public Optional<Double> getAllianceSkew(){
+        List<ColorBlobLocatorProcessor.Blob> blobs = allianceLocatorProcess.getBlobs();
+        if(blobs.isEmpty()){return Optional.empty();}
+
+        return Optional.of(getAngleFromRotatedRect(blobs.get(0).getBoxFit()));
+    }
+
+    private double getAngleFromRotatedRect(RotatedRect boxFitBlob){
         //This math is essentially to find the angle considering that the long side is vertical would be 0 degrees
         //This math is likely unecessary but just to be safe added
         Point[] vertices = new Point[4];
@@ -158,10 +241,16 @@ public class Vision extends SubsystemBase {
             angle = Math.atan((vertices[1].y - vertices[0].y)/(vertices[1].x - vertices[0].x));
         }
 
+        angle = Math.toDegrees(angle);
+        angle += 90;
+        if(angle>90){
+            angle-=180;
+        }
+
 //        telemetry.addData("side1", side1);
 //        telemetry.addData("side2", side2);
 //        telemetry.addData("side1longer", side1>side2);
-        return Optional.of(Math.toDegrees(angle));
+        return Math.toDegrees(angle);
     }
 
 }
